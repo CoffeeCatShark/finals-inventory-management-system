3268f37416385424b51fe1382df169fa9f5bb1a3
CoffeeStrike <omniexpansiontheory@gmail.com>
2025-05-01 18:36:21 +0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,20 +1,37 @@
 #include <iostream>
 #include <string>
-#include "Inventory.h"
-#include "OrderAndCart.h"
 #include "AccessControl.h"
+#include "Inventory.h"
+
 using namespace std;
 //readyItem() TO READY ADDING AN ITEM
 //inventory.deleteItem()
 //inventory.listItem()
 
-
+void menu(){
+		while(!validUser){
+		login();
+	}
+string input;	
+	cout<<"INVENTORY MANAGEMENT SYSTEM"<<endl;
+	cout<<"=================================="<<endl;
+	cout<<"[1]\tUpdate Item Quantities"<<endl;
+	cout<<"[2]\tRemove Item Quantities"<<endl;
+	if(adminControl == true){
+	cout<<"[3]\tAdd New Items"<<endl;
+	cout<<"[4]\tDelete Items from Inventory"<<endl;
+	}
+	
+	
+}
 
 int main(){
-
-	readyItem();
-	readyItem();
-	inventory.deleteItem("a");
-	inventory.listItem();
+	
+	menu();
+	
+	
+	
+	delete admin;
+	delete employee;
 	return 0;
 }
